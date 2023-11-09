fad9fe1ae320e98c546e8655a422f6335b3f1097
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2023-11-09 09:46:45 +0100

    page preferences created

--- src/screens/AccountSettingsScreen.js
+++ src/screens/AccountSettingsScreen.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
-import colors from '../configs/colors';
+import React, { useState } from 'react';
+import { View, StyleSheet, Switch, Text } from 'react-native';
+import { getAuth } from 'firebase/auth';
 import AppButton from '../components/AppButton';
 
 function AccountSettingsScreen({ navigation }) {
+
+    const auth = getAuth();
+    const [newsletter, setNewsletter] = useState(true);
+    const [privacySettings, setPrivacySettings] = useState(true);
+
+    const savePreferences = async () => {
+        try {
+            const user = auth.currentUser;
+        } catch (error) {
+            console.error('Error updating user settings:', error.message);
+        }
+    };
+
     return (
         <View style={styles.container}>
+            <View style={styles.preferenceItem}>
+                <Text>S'abonner à la newsletter</Text>
+                <Switch
+                    value={newsletter}
+                    onValueChange={(value) => setNewsletter(value)}
+                />
+            </View>
+
+            <View style={styles.preferenceItem}>
+                <Text>
+                    Paramètres de confidentialité
+                </Text>
+                <Switch
+                    value={privacySettings}
+                    onValueChange={(value) => setPrivacySettings(value)}
+                />
+            </View>
+            <AppButton title="Enregistrer mes préférences" onPress={() => savePreferences()} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    preferenceItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
 });
 
 export default AccountSettingsScreen;
